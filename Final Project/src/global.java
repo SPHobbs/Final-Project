e81209806390fb606697597bb340d765ba743f45
Sean Hobbs <seanphobbs@gmaill.com>
2015-04-28 21:26:05 -0400

    This is the almost completed version of the code

--- Final Project/src/global.java
+++ Final Project/src/global.java
@@ -1,5 +1,20 @@
 
 public class global extends javax.swing.JFrame {
 	public int price;
-	public javax.swing.JTextArea reciept;
+    public int pizza;
+    public int hotdog;
+    public int hamburger;
+    public int steak;
+    public int fries;
+    public int MAC;
+    public int biscuits;
+    public int greenBeans;
+    public int coke;
+    public int pepsi;
+    public int powerade;
+    public int gatorade;
+    public int iceCream;
+    public int brownie;
+    public int cookie;
+    public int cake;
 }
